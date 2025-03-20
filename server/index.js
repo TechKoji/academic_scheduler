40f2e611ea671134291d4e27968cae5f9639b9b3
Kojitha Gunathilake <kojithagunathilake@Kojithas-MacBook-Air.local>
2025-03-21 01:36:29 +0530

    fully setup server(backend) with mongodb connection

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
-import express from 'express'; 
+import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import connectDb from './db.js';
+
+dotenv.config(); // Loads .env variables
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
+connectDb();
+
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
-})
+});
